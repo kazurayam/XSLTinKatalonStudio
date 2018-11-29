efd18b46656366e2c188c60a3dbb39c086b26b90
kazurayam <kazuaki.urayama@gmail.com>
2018-11-29 13:03:11 +0900

    update

--- Keywords/XSLTHelper.groovy
+++ Keywords/XSLTHelper.groovy
@@ -1,24 +1,7 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-
-import com.kms.katalon.core.annotation.Keyword
-import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testobject.TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-
-import internal.GlobalVariable
-
-import javax.xml.transform.stream.StreamSource
+import javax.xml.transform.OutputKeys
 import javax.xml.transform.Transformer
 import javax.xml.transform.TransformerFactory
+import javax.xml.transform.stream.StreamSource
 
 public class XSLTHelper {
 
@@ -29,6 +12,8 @@ public class XSLTHelper {
 	static Transformer newTransformer(String stylesheet) {
 		Transformer transformer = TransformerFactory.newInstance().newTransformer(
 				new StreamSource(new StringReader(stylesheet)))
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
 		return transformer
 	}
 
--- Scripts/TC2_replaceOrgnlTxId/Script1543461730396.groovy
+++ Scripts/TC2_replaceOrgnlTxId/Script1543461730396.groovy
@@ -26,16 +26,32 @@ Path outputXML = projectDir.resolve('build/tmp/TC2_replaceOrgnlTxId.Output.xml')
 // create the identity transformer
 String stylesheet = '''
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
+    xmlns:CSC="urn:SctSCF:xsd:$SctScfBlkCdtTrf"
     xmlns="urn:iso:std:iso:20022:tech:xsd:camt.029.001.03">
-  <xsl:template match="@*|node()">
-    <xsl:copy>
-      <xsl:apply-templates select="@*|node()"/>
-    </xsl:copy>
+  <xsl:template match="/">
+    <xsl:apply-templates select="node()" />
   </xsl:template>
+  <xsl:template match="*">
+    <xsl:element name="{name()}">
+      <xsl:apply-templates select="@* | node()" />
+    </xsl:element>
+  </xsl:template>
+  <xsl:template match="@*">
+    <xsl:attribute name="{name()}">
+      <xsl:value-of select="." />
+    </xsl:attribute>
+  </xsl:template>
+  <xsl:template match="text()">
+    <xsl:if test="normalize-space()">
+      <xsl:value-of select="." />
+    </xsl:if>
+  </xsl:template>
+  
   <xsl:variable name="OrgnlTxId_value" select="'VAR2'" />
   <xsl:template match="OrgnlTxId">
-    <OrgnlTxId><xsl:value-of select="OrgnlTxId_value"/><OrgnlTxId>
+    <OrgnlTxId><xsl:value-of select="OrgnlTxId_value"/></OrgnlTxId>
   </xsl:template>
+
 </xsl:stylesheet>
 '''
 Transformer tr = CustomKeywords.'XSLTHelper.newTransformer'(stylesheet)
@@ -43,12 +59,10 @@ assert tr != null
 
 // load Input.xml
 String input = inputXML.toFile().getText()
-assert input != null
 
 // prepare Output.xml
 Files.createDirectories(outputXML.getParent())
 OutputStream os = new FileOutputStream(outputXML.toFile())
-assert os != null
 
 // perform XML Transformation
 tr.transform(new StreamSource(new StringReader(input)), new StreamResult(os))
